4c965dbb87eb744663e92d6fd66e2abaa9ea7b89
AdamCarrera <anc170430@utdallas.edu>
2021-02-07 16:05:54 -0600

    Update function 2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 def function1():
     print('goodbye')
 
-def function2():
-    print('Hello')
+
+def function2(x1, x2):
+    x3 = x1 * x2
+    return x1 * x2 + x3
 
 
 def main():
     function1()
-    function2()
+    y = function2(1, 3)
 
 
 if __name__ == '__main__':
