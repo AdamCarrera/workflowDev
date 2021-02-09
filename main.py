712fec488e61cd05800a5abb2fb9c284922b2084
AdamCarrera <anc170430@utdallas.edu>
2021-02-09 14:07:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ def function1():
     print('goodbye')
 
 
-
 def function2(x1, x2):
     x3 = x1 * x2
     # TEST COMMENTS!! CAN YOU READ THIS!!?
-    return x1 * x2 + x3
-
+    for x in x1:
+        print(x)
 
 
 def main():
     function1()
-    y = function2(1, 3)
-
+    function2([1, 2, 3], 3)
 
 
 if __name__ == '__main__':
